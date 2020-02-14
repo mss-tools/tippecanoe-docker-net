01069a5390b686378982f466575c3bba2fb4e9f9
Werner Mairl <werner.mairl@gmail.com>
2020-02-14 14:51:29 +0100

    refine!

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -e
+export PATH="$PATH:/root/.dotnet/tools"
 echo ""
-echo "Tippecanoe Runner CLI v1.0.4"
+echo "Tippecanoe Runner CLI v1.0.5"
 # SYNTAX
 #$1...geoJson file filter sample: "/*.orange.geojson"
 #$2...is the pure color name that can/should ne used for file or folder names!
@@ -37,12 +38,13 @@ fi
 
 mkdir -p $OUTER_FOLDER
 mkdir -p $OUTPUTDIRNAME
+mkdir -p ./splits
 
 #azureblob download --localdirectory $BITBUCKET_CLONE_DIR/splits --connectionstring $STORAGE_MSSDEV --container $CONTAINER_PRODUCTION --azuredirectory $SUBPATH_GEOJSON_PRODUCTION --mask "*.geojson"
 # ./cli-linux/docker-convert.sh "${BITBUCKET_CLONE_DIR}/splits/*.geojson" "outdoor" "${BITBUCKET_CLONE_DIR}/splits"
 
 echo "Start download from Azure Blob Container [${CONTAINER}] ${SUBPATH_GEOJSON} => ./splits"
-azureblob download --localdirectory ./splits --connectionstring $STORAGE --container $CONTAINER --azuredirectory $SUBPATH_GEOJSON --mask "*.geojson" --verbosity diag
+azureblob download --localdirectory ./splits --connectionstring $STORAGE --container $CONTAINER --azuredirectory $SUBPATH_GEOJSON --mask "*.geojson" --verbosity d
 echo ""
 echo "Download completed!"
 
@@ -59,21 +61,29 @@ if [ "${trace}" = "yes" ]; then
     echo "GEOJSON_LIST=${GEOJSON_LIST}"  
 fi;
 
+tippecanoe -v
+
 LAYER=mylayer
 
-i=0
+#i=0
+i=16
 while [ "$i" -le 18 ]; do
-  #echo "loop=$i"
+  echo "loop=$i"
   RENDER_ZOOM="-Z$i -z$i --drop-densest-as-needed"
-  OUTPUT_FILENAME="/data_tiles/$2_zoom_$i.mbtiles"
-  #echo "LOOP ENTER: ZOOM=${RENDER_ZOOM}, OUTPUT_FILENAME=${OUTPUT_FILENAME}, $2"
-  tippecanoe -v "$OUTER_FOLDER:$INNER_FOLDER" -v "$FOLDER:/splits"  $imageName -l ${LAYER} ${RENDER_ZOOM} -f -o ${OUTPUT_FILENAME} --quiet ${GEOJSON_LIST}
-  #echo ""
+  OUTPUT_FILENAME="./data_tiles/${SUBFOLDERCOLOR}_zoom_$i.mbtiles"
+  echo "LOOP ENTER: ZOOM=${RENDER_ZOOM}, OUTPUT_FILENAME=${OUTPUT_FILENAME}, ${SUBFOLDERCOLOR}"
+  tippecanoe ${RENDER_ZOOM} -f -o ${OUTPUT_FILENAME} --quiet ${GEOJSON_LIST}
+  echo ""
   i=$(( i + 1 ))
 done 
 
 if [ "${trace}" = "yes" ]; then
   echo ""
-  echo "show content of tiles folder"
+  echo "show content of tiles folder (${OUTER_FOLDER})"
   ls $OUTER_FOLDER
 fi
+
+echo "Start upload to Azure Blob Container ./data_tiles => [${CONTAINER}] ${SUBPATH_MBTILES}" 
+azureblob upload --localdirectory ./data_tiles --connectionstring $STORAGE --container $CONTAINER --azuredirectory $SUBPATH_MBTILES --mask "*.mbtiles"
+echo ""
+echo "upload completed!"
